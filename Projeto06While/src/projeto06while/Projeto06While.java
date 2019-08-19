d393dd0eec965cc0fdd930f656be157f2d9ec294
Polianyfernands2 <54293223+Polianyfernands2@users.noreply.github.com>
2019-08-19 20:05:58 -0300

    Add files via upload

---
+++ Projeto06While/src/projeto06while/Projeto06While.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto06while;
+
+/**
+ *
+ * @author aluno
+ */
+public class Projeto06While {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // de 10 a 0
+        int cont = 10;
+        while (cont >= 0){
+            System.out.println("Valor de cont:" + cont);
+            cont = cont + 1;
+        }
+    }
+    
+}
